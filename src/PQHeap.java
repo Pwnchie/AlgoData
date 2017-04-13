f41b3d8857dacc2c55994180993516d1389329a7
Pwnchie <larstrierstorm@hotmail.com>
2017-04-14 01:55:48 +0200

    New stuff

--- src/PQHeap.java
+++ src/PQHeap.java
@@ -9,25 +9,31 @@ import java.util.ArrayList;
 public class PQHeap implements PQ {
 
     //Variables for internal use in the class.
-   private ArrayList<Element> myheap = new ArrayList<>();
+
    private Element minimum;
+   private PQHeap myheap;
+   private ArrayList<Element> myarray;
 
     //Ensures that the ArrayList has the desired size of maxElms.
     //Adds one to desired length to compensate for starting at
     //array index 1 instead of 0.
     public PQHeap(int maxElms) 
     {
-        myheap.ensureCapacity(maxElms+1);
+        ArrayList<Element> newarray = new ArrayList<>();
+        newarray.ensureCapacity(maxElms+1);
+
+        myarray = newarray;
     }
+
     //Extracts from the heap the element with the lowest key
     //and trims the array to fit the new size of the array.
     public Element extractMin() {
-        if (myheap.size() < 1) {
+        if (myheap.myarray.size() < 1) {
             System.out.println("Heap underflow");
         } else {
-            minimum = myheap.get(0);
-            myheap.remove(0);
-            myheap.trimToSize();
+            minimum = myheap.myarray.get(1);
+            myheap.myarray.remove(1);
+            myheap.myarray.trimToSize();
         }
         return minimum;
     }
@@ -36,35 +42,57 @@ public class PQHeap implements PQ {
     //Within the heap.
     public void insert(Element e) {
         Element newInsert = e;
-        myheap.add(1, newInsert);
-        minHeapify(myheap, 1);
+        myheap.myarray.add(0, newInsert);
+        minHeapify(myheap.myarray, 0);
     }
-    
+
+    //New method for maintaining a heapstructure via substitution
+    private void maintainMinHeap()
+    {
+        int key;
+        int i;
+        Element temp;
+
+         key = myheap.myarray.get(0).key;
+         if(myheap.myarray.get(1) == null)
+         {
+             myheap.myarray.set(1,myheap.myarray.get(0));
+         }
+         else
+             i = 1;
+             while (i < myheap.myarray.size())
+             {
+                 if (myheap.myarray.get( < )
+                 {
+
+                 }
+             }
+    }
     //Enforces heapstructure by sorting contents of and arraylist.
     private void minHeapify(ArrayList<Element> a, int i) {
-        a = myheap;
+        a = myheap.myarray;
         //i = i + 1
         int smallest;
         int l = leftChildOf(i);
         int r = rightChildOf(i);
-        Element parent = myheap.get(parentOf(i));
-        Element leftchild = myheap.get(leftChildOf(i));
-        Element rightchild = myheap.get(rightChildOf(i));
+        Element parent = myheap.myarray.get(parentOf(i));
+        Element leftchild = myheap.myarray.get(leftChildOf(i));
+        Element rightchild = myheap.myarray.get(rightChildOf(i));
 
-        if (i <= myheap.size() - 1 && parent.key >= leftchild.key) {
+        if (i <= myheap.myarray.size() - 1 && parent.key >= leftchild.key) {
             smallest = l;
         } else {
             smallest = i;
         }
 
-        if (i <= myheap.size() - 1 && parent.key >= rightchild.key) {
+        if (i <= myheap.myarray.size() - 1 && parent.key >= rightchild.key) {
             smallest = r;
         }
 
         if (smallest != i) {
-            Element temp = myheap.get(i);
-            myheap.set(i, myheap.get(smallest));
-            myheap.set(smallest, temp);
+            Element temp = myheap.myarray.get(i);
+            myheap.myarray.set(i, myheap.myarray.get(smallest));
+            myheap.myarray.set(smallest, temp);
             minHeapify(myheap, smallest);
         }
 
